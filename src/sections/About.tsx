591a4fb0c2ce73bf2884a503622da95d9f789b54
Vnb123 <vanabg@yahoo.com>
2025-10-27 16:47:59 +0000

    Finished initial project setup for the portfolio website. Deleted vite setup template and created a sections folder connecting to the App.tsx

---
+++ src/sections/About.tsx
@@ -0,0 +1,4 @@
+function About() {
+  return <h2>About me</h2>;
+}
+export default About;
---
+++ src/sections/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+      <h1>Vana Bigava</h1>
+      <h2>Junior Software Engineer</h2>
+    </div>
+  );
+}
+export default Home;
