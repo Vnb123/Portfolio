3c559c1bafa6a4aa7aace3765076960955b81451
Vnb123 <vanabg@yahoo.com>
2025-11-05 23:49:29 +0000

    Created a navigation component for single page navigation

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+function Nav() {
+  return (
+    <nav>
+      <a href="#about">About Me</a>
+      <a href="#projects">Projects</a>
+      <a href="#contact">Contact</a>
+    </nav>
+  );
+}
+export default Nav;
