e84e820f22728e728b7bdcc94651e9f7f82629bf
Vnb123 <vanabg@yahoo.com>
2025-11-13 18:11:24 +0000

    fixed imports so only the Home page was in the App.tsx and the rest off the sections were inside the Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import About from "./sections/About";
-import Contact from "./sections/Contact";
 import Home from "./sections/Home";
-import Projects from "./sections/Projects";
 
 function App() {
   return (
     <>
       <Home />
-      <About />
-      <Projects />
-      <Contact />
     </>
   );
 }
